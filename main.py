2d068f289e8e3af9a984f4756e30432111256203
Kaiden A <69645495+kapplewhaite@users.noreply.github.com>
2022-11-29 10:13:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a = np.array(
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
@@ -11,3 +12,8 @@ b = np.array(
 )
 print(a)
 print(b)
+
+plt.plot(a, b)
+plt.title('Average Temperature in January (2021)')
+plt.ylabel('Average Temperature')
+plt.show()
