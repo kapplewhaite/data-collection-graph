478704b707f54a034b1c4223b79e32195cda7bc5
Kaiden A <69645495+kapplewhaite@users.noreply.github.com>
2022-11-30 10:28:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = np.array(
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
     dtype='i'
 )
 
 b = np.array(
-    [63.4, 58.4, 45.1, 45.7, 48.8, 43.5, 41.7, 39.7, 35.6, 38.5, 42.0, 41.0, 38.8, 44.0, 47.5, 37.5, 41.4, 43.5, 48.3, 49.6, 48.9, 47.9, 48.1, 51.7, 56.4, 64.2, 53.7, 39.7, 39.4, 41.8, 44.2], 
+    [49.28, 49.5, 60.82, 61.56, 68.52, 75.73, 81.06, 79.21, 72.83, 66.67, 58.37, 46.06, 46.13, 47.58, 59.07, 62.67, 69.57, 76.49, 78.65, 78.91, 73.49, 66.18, 52.42, 55.5], 
     dtype='f'
 )
 print(a)
 print(b)
 
 plt.plot(a, b, label = "original")
-plt.title('Average Temperature in January (2021)')
-plt.xlabel('Days')
+plt.title('Average Temperature Over 2 Years (2020 - 2021)')
+plt.xlabel('Months (1 - 24)')
 plt.ylabel('Average Temperature')
 plt.legend()
 plt.show()
