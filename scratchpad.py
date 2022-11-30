7fa19831588e812ac5818ab5545d7791c4f0f2d1
Kaiden A <69645495+kapplewhaite@users.noreply.github.com>
2022-11-30 14:21:43 -0500

    Update scratchpad.py

--- scratchpad.py
+++ scratchpad.py
@@ -3,3 +3,12 @@ import numpy as np
 # Imports Matplotlib to plot and create a graph from the arrays
 import matplotlib.pyplot as plt
 
+# Sets evenly spaced points from 1 to 24 to represent months
+c = np.linspace(1, 24, 100)
+
+# A sine equation that predicts the average temperature over 2 years
+d = 18.9 * np.sin((np.pi / 12) * c - (np.pi / 12)) + 61.7
+
+# Plots the data points
+plt.plot(c, d, label = "Prediction")
+plt.show()
