10ec55bf39d4f878db58ccbc33c8b3d17ca6ba10
Kaiden A <69645495+kapplewhaite@users.noreply.github.com>
2022-11-30 14:21:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,21 @@ b = np.array(
     [49.28, 49.5, 60.82, 61.56, 68.52, 75.73, 81.06, 79.21, 72.83, 66.67, 58.37, 46.06, 46.13, 47.58, 59.07, 62.67, 69.57, 76.49, 78.65, 78.91, 73.49, 66.18, 52.42, 55.5], 
     dtype='f'
 )
-print(a)
-print(b)
 
 # Plots the data points
-plt.plot(a, b, label = "original")
+plt.plot(a, b, label = "Original")
 plt.title('Average Temperature Over 2 Years (2020 - 2021)')
 plt.xlabel('Months (1 - 24)')
 plt.ylabel('Average Temperature')
+
+# Sets evenly spaced points from 1 to 24 to represent months
+c = np.linspace(1, 24, 100)
+
+# A sine equation that predicts the average temperature over 2 years
+d = 18.9 * np.sin((np.pi / 12) * c - (np.pi / 12)) + 61.7
+
+# Plots the data points
+plt.plot(c, d, label = "Prediction")
 
 # Creates a legend to use on the graph
 plt.legend()
